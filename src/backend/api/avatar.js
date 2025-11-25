432eca022cf1e7476decc74f1954abad980b4873
Dennis Gamayon <gamayon.dennis8@gmail.com>
2025-11-25 14:34:57 +0800

    Placeholder image for unauthenticated user

--- src/backend/api/avatar.js
+++ src/backend/api/avatar.js
@@ -1,3 +1,6 @@
 export const getAvatarUrl = (seed) => {
+    if (!seed || seed === 'anonymous') {
+        return 'https://api.dicebear.com/9.x/bottts/svg?seed=anonymous';
+    }
     return `https://api.dicebear.com/9.x/avataaars/svg?seed=${seed}`;
 };
